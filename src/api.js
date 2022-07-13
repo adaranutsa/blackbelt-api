68f08e016bc2adb9a526ae72f97025cfdd83b412
aleksdaranutsa <aleksdaranutsa@gmail.com>
2022-07-13 14:52:01 -0700

    Adding cores to allow web to make requests

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 80
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send({message: 'Hello World!'});
